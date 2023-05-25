9829fd69b5312eb4964b6781cb7c5c8ee98b3287
mikeredev <mikeredev@gmail.com>
2023-05-26 00:51:32 +0100

    updates

--- manage-session-rofi.py
+++ manage-session-rofi.py
@@ -2,26 +2,37 @@
 
 import subprocess
 
-options = ["Lock", "Logoff", "Suspend", "Reboot", "Shutdown"]
+options = ["lock", "logoff", "suspend", "reboot", "shutdown"]
 chosen = subprocess.check_output(
-    ["rofi", "-dmenu", "-i", "-p", "Manage Session"],
+    ["rofi", "-dmenu", "-i", "-p", "session"],
     input="\n".join(options),
     text=True,
 ).strip()
 
-if chosen == "Lock":
-    # lock the screen using i3lock
+if chosen == "lock":
     subprocess.run(["i3lock"])
-elif chosen == "Logoff":
-    # log off the current session using i3-msg
-    subprocess.run(["i3-msg", "exit"])
-elif chosen == "Suspend":
-    # lock the screen using i3lock
+elif chosen == "logoff":
+    confirm = subprocess.check_output(
+        [
+            "rofi",
+            "-dmenu",
+            "-i",
+            "-p",
+            "Are you sure you want to logoff? [y/n]",
+            "-lines",
+            "2",
+            "-eh",
+            "2",
+        ],
+        input="y\nn",
+        text=True,
+    ).strip()
+    if confirm.lower() == "y":
+        subprocess.run(["i3-msg", "exit"])
+elif chosen == "suspend":
     subprocess.run(["i3lock"])
-    # suspend the computer using systemctl
     subprocess.run(["systemctl", "suspend"])
-elif chosen == "Reboot":
-    # prompt the user for confirmation using rofi
+elif chosen == "reboot":
     confirm = subprocess.check_output(
         [
             "rofi",
@@ -38,10 +49,8 @@ elif chosen == "Reboot":
         text=True,
     ).strip()
     if confirm.lower() == "y":
-        # reboot the computer using systemctl
         subprocess.run(["systemctl", "reboot"])
-elif chosen == "Shutdown":
-    # prompt the user for confirmation using rofi
+elif chosen == "shutdown":
     confirm = subprocess.check_output(
         [
             "rofi",
@@ -58,8 +67,6 @@ elif chosen == "Shutdown":
         text=True,
     ).strip()
     if confirm.lower() == "y":
-        # shut down the computer using systemctl
         subprocess.run(["systemctl", "poweroff"])
 else:
-    # do nothing if an invalid option is selected
     pass
