d7b968a6dc322865b560403c873685348c80b6dc
mikeredev <mikeredev@gmail.com>
2023-06-22 18:57:05 +0100

    external modules added

--- backup-dotfiles.py
+++ backup-dotfiles.py
@@ -1,33 +1,22 @@
-import os
-import json
-import filecmp
-import shutil
+# import modules
+from colorama import Style
 import datetime
+import filecmp
+import json
+import os
+import shutil
 import sys
 
-color_reset = "\033[0m"
-color_ok = "\033[32m"
-color_nok = "\033[31m"
-color_log = "\033[2m"
+# import custom modules from PYTHONPATH
+import run_function
 
+# define constants
 HOME_DIR = os.path.expanduser("~")
 CONFIG_FILE = f"{HOME_DIR}/.config/system-mgmt/backup-dotfiles.json"
 BACKUP_DIR = f"{HOME_DIR}/data/backup/dotfiles"
 
 
-def run_function(message, func, *args, **kwargs):
-    output = f"=> {message}... "
-    print(output, end="")
-    try:
-        result = func(*args, **kwargs)
-        result = f"{color_ok}OK{color_reset} {color_log}{str(result)}{color_reset}"
-        print(result)
-    except Exception as e:
-        result = f"{color_nok}FAIL{color_reset}\n{color_log}==> {str(e)}{color_reset}"
-        print(result)
-        sys.exit(2)
-
-
+# function to load CONFIG_FILE
 def load_configuration():
     global files_for_backup
     func_output = ""
@@ -44,6 +33,7 @@ def load_configuration():
     return func_output
 
 
+# function to check backup folder directory structure
 def check_backup_structure():
     func_output = ""
     # make parent dir if not existing
@@ -60,6 +50,7 @@ def check_backup_structure():
     return func_output
 
 
+# function to compare files listed in CONFIG_FILE
 def check_backup_status():
     backups = {}
     for source_path, dest_path in files_for_backup:
@@ -72,6 +63,7 @@ def check_backup_status():
     return backups
 
 
+# function to copy any changed files to BACKUP_DIR
 def make_backup():
     func_output = ""
     backups = check_backup_status()
@@ -83,7 +75,7 @@ def make_backup():
     func_output_log = "new files found"
     for source_path, dest_path in backups.items():
         func_output_log += f"\n==> {source_path} -> {dest_path}"
-    print(f"{color_log}{func_output_log}{color_reset}")
+    print(f"{Style.DIM}{func_output_log}{Style.RESET_ALL}")
 
     # get user confirmation to continue
     print(f"=> do you want to backup these files? (y/n) ", end="")
@@ -106,7 +98,8 @@ def make_backup():
     return func_output
 
 
+# main
 if __name__ == "__main__":
-    run_function("load config", load_configuration)
-    run_function("initialise backup", check_backup_structure)
-    run_function("execute backup", make_backup)
+    run_function.run(load_configuration, "load config")
+    run_function.run(check_backup_structure, "initialise backup")
+    run_function.run(make_backup, "execute backup")
