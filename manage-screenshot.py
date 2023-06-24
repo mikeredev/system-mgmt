fceb2fd142f492c7f621824fc9c0aaca7ec4abfc
mikeredev <mikeredev@gmail.com>
2023-06-24 04:43:58 +0100

    comments

--- manage-screenshot.py
+++ manage-screenshot.py
@@ -1,3 +1,10 @@
+""" manage-screenshot.py
+desc:                   for taking screenshots via hotkey and saving
+i3 full screen shot:    bindsym $mod+Print exec --no-startup-id sh -c "python ~/data/scripts/system-mgmt/manage-screenshot.py --type full"
+i3 area screenshot:     bindsym $mod+Shift+Print exec --no-startup-id sh -c "sleep 0.2s && python ~/data/scripts/system-mgmt/manage-screenshot.py --type area"
+"""
+
+# import modules
 import os
 import random
 import time
@@ -5,12 +12,14 @@ import subprocess
 import argparse
 
 
+# function to copy image to clipboard
 def copy_to_clipboard(image_path):
     subprocess.run(
         ["xclip", "-selection", "clipboard", "-t", "image/png", image_path], check=True
     )
 
 
+# function to take screenshot (fullscreen / area)
 def take_screenshot(screenshot_type="full"):
     date = time.strftime("%Y-%m-%d")
     rand_num = str(random.randint(10000, 99999))
@@ -23,6 +32,7 @@ def take_screenshot(screenshot_type="full"):
     copy_to_clipboard(file_path)
 
 
+# main
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Take a screenshot of the full screen or a selected area and copy it to the clipboard"
